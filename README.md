c264b8679e0ba43e0ded0039e922d33adae59f08
Jean <jeanmacario048@gmail.com>
2025-02-19 15:27:57 -0600

    FIRST COMMIT

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sistema de Gestion de Gastos y Facturas
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return "Sistema de Gestion de Gastos y Facturas";
+});
