16ad18b706688500efeff0bcecfab1bb6544efc5
Jean <jeanmacario048@gmail.com>
2025-04-08 10:23:40 -0600

    Build\WO\ Added 4 banks view 08.04.2025-1022

---
+++ public/build/assets/AuthenticationCardLogo-D1If9565.js
@@ -0,0 +1,1 @@
+import{c as s,o as a,w as o,a as t,u as r,C as n}from"./app-z4zDQCyt.js";const u={__name:"AuthenticationCardLogo",setup(c){return(i,e)=>(a(),s(r(n),{href:"/"},{default:o(()=>e[0]||(e[0]=[t("img",{class:"size-28",src:"/assets/logo.jpeg",alt:"Logo"},null,-1)])),_:1}))}};export{u as _};
